08f639e6f7ec7703481c64d93d98afa885d6b141
Caio Oliveira <caio.s.o@me.com>
2020-12-16 21:16:07 +0100

    Implent basic interface of Memory Controller.

--- src/MemoryController.h
+++ src/MemoryController.h
@@ -14,7 +14,13 @@ public:
     MemoryController() = default;
     ~MemoryController() = default;
 
+    void WriteByte(uint8_t value, uint16_t address);
+    void WriteWord(uint16_t value, uint16_t address);
+    uint8_t ReadByte(uint16_t address);
+    uint16_t ReadWord(uint16_t address);
+
     void RegisterMemoryResource(std::shared_ptr<Memory> resource, AddressRange range);
+    void UnregisterMemoryResource(std::shared_ptr<Memory> resource);
 };
 
 }
