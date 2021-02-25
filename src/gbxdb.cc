bbfe05cc22f622564c911fe897b1202d02126508
Caio Oliveira <caio.s.o@me.com>
2021-02-25 20:31:53 +0100

    Proof-of-concept for remote debug server interation

--- src/gbxdb.cc
+++ src/gbxdb.cc
@@ -2,10 +2,10 @@
 #include <iostream>
 #include <sstream>
 
-using namespace boost::asio;
-using ip::tcp;
+using namespace boost;
 using std::string;
 using std::cout;
+using std::cin;
 using std::endl;
 
 int main ()
@@ -13,9 +13,35 @@ int main ()
     boost::asio::io_service io_service;
     cout << "GAME BOY X Debugger" << '\n' ;
 
-    tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234 ));
-    tcp::socket socket_(io_service);
-    acceptor_.accept(socket_);
+    std::string raw_ip_address = "127.0.0.1";
+    unsigned short port_num = 5678;
 
-    cout << "Connection Accepted" << '\n';
+    try 
+    {
+        asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address), port_num);
+        asio::io_service ios;
+
+        asio::ip::tcp::socket sock(ios, ep.protocol());
+
+
+        cout << "Ready to connect to server..." << '\n';
+        sock.connect(ep);
+        cout << "Connection established!" << '\n';
+
+        for (;;)
+        {
+            std::string message;
+            cin >> message;
+
+            cout << "message to send: " << message << '\n'; 
+
+            boost::system::error_code ignored_error;
+            boost::asio::write(sock, boost::asio::buffer(message), ignored_error);
+        }
+    }
+    catch (system::system_error &e) 
+    {
+        std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what();
+    }
+
 }
